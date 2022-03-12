bd97506b2291a8833b2ad9c8d3011a2e829234c9
Samuli Öhman <samuli.ohman@aalto.fi>
2022-03-12 17:58:12 +0200

    Added scala-swing to sbt. Created a basic window with some buttons and text boxes.

--- src/main/scala/GUI.scala
+++ src/main/scala/GUI.scala
@@ -1,4 +1,32 @@
-object GUI extends App{
-  println("Hello")
-  println("It works")
+import scala.swing._
+import scala.swing.event.ButtonClicked
+
+object GUI extends SimpleSwingApplication {
+  val nappula = new Button("Nappi")
+  val textField = new TextField("Default input", 50)
+  val textArea = new TextArea("Default text", 90, 90)
+
+  val horizontalPanel = new BoxPanel(Orientation.Horizontal)
+  horizontalPanel.contents += new Label("Here we have a text box")
+  horizontalPanel.contents += textField
+  horizontalPanel.contents += nappula
+
+  val fullPanel = new BoxPanel(Orientation.Vertical)
+  fullPanel.contents += horizontalPanel
+  fullPanel.contents += textArea
+  fullPanel.preferredSize = new Dimension(1280,720)
+
+  val window = new Frame
+  window.title = "Solar simulator"
+  window.contents = fullPanel
+
+  this.listenTo(nappula)
+  this.reactions += {
+    case painallus: ButtonClicked =>
+      val lahdenappula = painallus.source
+      val textInBox = textField.text
+      textArea.text = textInBox
+  }
+
+  override def top: Frame = window
 }
